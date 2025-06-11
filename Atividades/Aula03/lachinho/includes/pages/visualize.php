8732b07f0ce2d5ba933643a22d593cae3f9b619e
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-06-11 13:43:30 -0300

    update the visualize page

--- Atividades/Aula03/lachinho/includes/pages/visualize.php
+++ Atividades/Aula03/lachinho/includes/pages/visualize.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar Produto</title>
     <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/visualize.css"> 
 </head>
 <body>
     <nav>
@@ -14,6 +15,40 @@
             <a class="navLink" href="visualize.php"><h1>Visualizar Produto</h1></a>
         </div>
     </nav>
+
+    <div class="table-div">
+         <table>
+            <thead>
+                <td>ID</td>
+                <td>Nome</td>
+                <td>Descrição</td>
+                <td>Preço</td>
+                <td>Ações</td>
+            </thead>
+            <tr>
+                <td>0</td>
+                <td>Apple</td>
+                <td>Apenas uma simple maçã</td>
+                <td>4,00</td>
+                <td>Remover | Editar</td>
+            </tr>
+             <tr>
+                <td>1</td>
+                <td>Baked Potato</td>
+                <td>Uma batata assada</td>
+                <td>5,00</td>
+                <td>Remover | Editar</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Beetroot</td>
+                <td>Uma Beterraba</td>
+                <td>1,00</td>
+                <td>Remover | Editar</td>
+            </tr>
+         </table>
+    </div>
+   
     
 </body>
 </html>
