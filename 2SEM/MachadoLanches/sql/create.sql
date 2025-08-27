83a1ff583ec2d200ad3cd3ea173732c53b52b638
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-08-27 13:22:37 -0300

    tables mesa and pedidos

--- 2SEM/MachadoLanches/sql/create.sql
+++ 2SEM/MachadoLanches/sql/create.sql
@@ -10,12 +10,35 @@ CREATE TABLE funcionarios (
     ativo BOOLEAN NOT NULL DEFAULT TRUE,
 );
 
-CREATE TABLE client (
+CREATE TABLE produto (
+    id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    preco DECIMAL(4,2) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL
+)
+
+
+CREATE TABLE clientes (
     id_cliente INT PRIMARY KEY AUTO_INCREMENT,
     nome_cliente VARCHAR(255) NOT NULL,
     telefone VARCHAR(255) NOT NULL,
     endereco VARCHAR(255) NOT NULL,
     sexo ENUM('M', 'F') NOT NULL DEFAULT ('F'),
     data_cadastro DATE NOT NULL
+);
+
+CREATE TABLE mesas (
+    id_mesa INT PRIMARY KEY AUTO_INCREMENT,
+    quantidade_pessoas INT NOT NULL,
+    horario_entrada DATETIME NOT NULL,
+    horario_saida DATETIME NOT NULL,
+    vazia BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE DATABASE pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_mesa INT NOT NULL,
+    id_produto INT NOT NULL,
+    FOREIGN KEY (id_mesa) REFERENCES mesas(id_mesa)
 );
 
