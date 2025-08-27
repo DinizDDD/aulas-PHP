6cc46dddefc836a62c8115a3aa4ddd8be612612f
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-08-27 13:06:35 -0300

    make sql for client and funcionario

---
+++ 2SEM/MachadoLanches/sql/create.sql
@@ -0,0 +1,21 @@
+DROP DATABASE MachadoLanches;
+CREATE DATABASE MachadoLanches;
+USE MachadoLanches;
+
+CREATE TABLE funcionarios (
+    id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
+    nome_funcionario VARCHAR(255) NOT NULL,
+    cargo VARCHAR(255) NOT NULL,
+    data_admisssao DATE NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+);
+
+CREATE TABLE client (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cliente VARCHAR(255) NOT NULL,
+    telefone VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    sexo ENUM('M', 'F') NOT NULL DEFAULT ('F'),
+    data_cadastro DATE NOT NULL
+);
+
